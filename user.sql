2d1ca6630ee44be6cbff2d0fc4818ddc09972189
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-06-03 01:20:51 +0900

    feat: sql auto_increment 자료형 수정

--- user.sql
+++ user.sql
@@ -1,12 +1,5 @@
 -- db name: 404blog
 
-create table user_info (
-    idx varchar(255) auto_increment,
-    id varchar(255) not null,
-    name varchar(255) not null,
-    pw varchar(255) not null,
-    tel varchar(255) not null,
-    primary key(idx, id)
-)
+ 
 
 
