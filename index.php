34609bb9198116c4edf8eb1e5430701c56f74719
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-06-03 01:19:16 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 blog</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <div class="headerInner">
+            <div class="logo">
+                <img src="#" alt="404blog_main_logo" title="404blog_main_logo">
+            </div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            <ul class="util">
+                <li class="login"><a href="/page/login.php">로그인</a></li>
+                <li class="join"><a href="/page/join.php">회원가입</a></li> 
+                <li class="myPage"><a href="/page/mypage.php">마이페이지</a></li> 
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
