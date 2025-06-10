52c325fdc22642826b3c7cd6ac435e0b44fe76c2
choitjddn0311 <cswcjw311427@naver.com>
2025-06-10 15:41:50 +0900

    feat: 소개 페이지 추가

---
+++ frontend/src/page/intro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Intro = () => {
+    return (
+        <>
+            <h1>개발자를 꿈꿉니다</h1>
+            <h1>최성우</h1>
+        </>
+    )   
+}
+
+export default Intro;
