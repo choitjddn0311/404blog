6434504e75530cf705deb47500e3159695fbc083
choitjddn0311 <cswcjw311427@naver.com>
2025-06-10 15:40:12 +0900

    docs: sql 접속 방법 추가

--- backend/db/user.sql
+++ backend/db/user.sql
@@ -1,5 +1,8 @@
 -- db 접속
--- \connect --mysql root@localhost:3306
+-- \connect --mysql root@localhost:3306 
+-- 또는
+-- c -> program file -> mysql -> mysql server 8.0 -> bin = cmd ->
+-- mysql -u root -p -> pw입력후 진입입
 
 create table user (
     idx int auto_increment,
