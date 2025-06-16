24b67381c329cb0373eaced4b7e9e672e3ac904d
choitjddn0311 <cswcjw311427@naver.com>
2025-06-16 16:44:51 +0900

    feat: post 불러오기

---
+++ backend/routes/post.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+
+router.get('/' , async (req,res) => {
+    try {
+        const [rows] = await db.query('select * from post');
+        res.json(rows);
+    } catch(err) {
+        console.error("게시물 불러오기 실패했슈" , err);
+        res.status(500).json({message: '서버 오류때문이랑께'});
+    }
+})
+
+module.exports = router;
