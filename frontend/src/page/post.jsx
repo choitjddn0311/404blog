eeae93909833a2137f5bacbf09b26e8d61d9e4a4
choitjddn0311 <cswcjw311427@naver.com>
2025-06-16 18:31:40 +0900

    feat: post 영역 잡기

--- frontend/src/page/post.jsx
+++ frontend/src/page/post.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+
+const containerSize = 1400;
+const mainColor = '#fb8500';
+
+const Main = styled.main`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #ddd;
+`;
+
+const Container = styled.div`
+    width: ${containerSize}px;
+    background: #aaa;
+`;
+
+const Form = styled.form`
+    width: 100%;
+    height: 90vh;
+    position: sticky;
+    top: 0;
+    background: ${mainColor};
+    overflow-y: scroll;
+
+    -ms-overflow-style: none;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
 
 const Post = () => {
-    return (
+    return(
         <>
-            <h1>post 전용입니다</h1>
+            <Main>
+                <Container>
+                    <Form></Form>
+                </Container>
+            </Main>
         </>
     )
 }
+
 export default Post;
