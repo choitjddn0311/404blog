031bc89438fb20912ab390f4e280e4d19af0c24b
choitjddn0311 <cswcjw311427@naver.com>
2025-06-16 17:51:58 +0900

    feat: 페이지 접근에 관한 pageAlert 컴포넌트

---
+++ frontend/src/components/alert/pageAlert.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const AlertDisplay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  height: 100vh;
+  background-color: rgba(0,0,0,0.5);
+  color: #fff;
+  z-index: 1001;
+  font-size: 80px;
+  font-weight: bold;
+  text-align: center;
+  align-content: center;
+`;
+
+const PageAlert = ({ message, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose(); // 3초 후 닫기
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return <AlertDisplay>{message}</AlertDisplay>;
+};
+
+export default PageAlert;
