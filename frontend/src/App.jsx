de5f019b27a63f421f7feb59eb2de8972ed9aa4b
choitjddn0311 <cswcjw311427@naver.com>
2025-06-16 13:43:33 +0900

    feat: mypage 라우터 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header";
 import Home from "./page/home";
 import Post from "./page/post";
 import Intro from "./page/intro";
+import Mypage from "./page/myPage";
 import GlobalStyle from "./style/globalStyle";
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home />}></Route>
           <Route path="/post" element={<Post />}></Route>
           <Route path="/intro" element={<Intro />}></Route>
+          <Route path="/mypage" element={<Mypage />}></Route>
         </Routes>
     </BrowserRouter>
     </>
