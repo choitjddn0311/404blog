0622c06356be59307684105bd0cbf8fda7e45297
choitjddn0311 <cswcjw311427@naver.com>
2025-06-17 16:38:23 +0900

    feat: test data

---
+++ backend/db/testData.sql
@@ -0,0 +1,8 @@
+-- user table
+insert into user (id, name, pw) values ('choi', '관리자', '2222');
+
+-- post table
+insert into post (title, content) values ('테스트 글 제목', '테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.테스트중입니다 감사합니다. 테스트중입니다 감사합니다.')
+insert into post (id, title, content, post_text) values ('admin' , '글 제목 부분입니다.' , '글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.글 내용입니다.' , '글 내용에 대해 알아봤습니다');
+insert into post (id, title, content, post_text) values ('admin' , 'db 이모티콘 테스트입니다' , '😊😊😊' , '과연');
+insert into post (id, title, content, post_text) values ('admin' , '시간정렬 테스트입ㄴ다' , 'gjgjgj' , 'gjgjgjgjgj');
