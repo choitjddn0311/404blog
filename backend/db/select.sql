9c925df0353695be4689abb048eb2d28ae208178
choitjddn0311 <cswcjw311427@naver.com>
2025-06-17 16:39:06 +0900

    feat: 필요한 select 문 파일 생성

---
+++ backend/db/select.sql
@@ -0,0 +1,9 @@
+-- 임시저장글 가져오기
+select title, content from temporaily_post where id = '로그인된 아이디';
+
+-- 내 글 가져오기 (화면에)
+select title, post_text from post where id = '로그인된 아이디';
+
+-- 최신 포스팅으로 정렬하기
+select * from post order by created_at desc;
+
