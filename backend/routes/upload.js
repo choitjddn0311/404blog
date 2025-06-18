216c8acc25da42e04b07ad5f19101822d445fd3b
choitjddn0311 <cswcjw311427@naver.com>
2025-06-18 13:57:27 +0900

    docs: 백엔드 코드 주석

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const db = require('../config/db')
 
 router.post('/' , async(req,res) => {
+    // id, 제목, 내용, 보일 글
     const {id, title, content, post_text} = req.body;
-    console.log('업로드데이터' , req.body);
     
+    // 유효성체크
     if(!id || !title || !content || !post_text) {
         return res.status(400).send("모든 항목의 값을 입력해주세요");
     }
 
+    // 글 개시 insert
     const sql = `
         insert into post (id, title, content, post_text) values (?,?,?,?)
     `;
