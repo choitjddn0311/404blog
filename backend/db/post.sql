5fbf2b38c259a91865fb5c7a1f7e6005416fce15
choitjddn0311 <cswcjw311427@naver.com>
2025-06-23 17:13:26 +0900

    fix(db): increase column size for title in post.sql

--- backend/db/post.sql
+++ backend/db/post.sql
@@ -1,7 +1,7 @@
 create table post (
     idx int auto_increment primary key,
     id varchar(255) not null,
-    title varchar(255) not null,
+    title varchar(396) not null,
     content varchar(10000) not null,
     post_text varchar(500) not null,
     created_at datetime not null default current_timestamp
