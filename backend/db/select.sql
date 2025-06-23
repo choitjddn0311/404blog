d1b519d7799df98bf1b9e58a987933ad300ad055
choitjddn0311 <cswcjw311427@naver.com>
2025-06-23 16:35:25 +0900

    feat(db): add query to fetch posts with user names by title

--- backend/db/select.sql
+++ backend/db/select.sql
@@ -7,3 +7,5 @@ select title, post_text from post where id = '로그인된 아이디';
 -- 최신 포스팅으로 정렬하기
 select * from post order by created_at desc;
 
+-- 내 글 가져오기 (아이디랑 같이)
+select p.id, u.name, p.title, p.content, p.created_at from post p, user u where p.id = u.id  and title = '클릭된 카드의 타이틀'
