dc23e3fa1b3510aba19c74aa8af64d75c44f1279
choitjddn0311 <cswcjw311427@naver.com>
2025-06-23 14:21:33 +0900

    feat: post card 클릭시 해당하는 게시물이 보이게 구성

--- frontend/src/page/postShow.jsx
+++ frontend/src/page/postShow.jsx
@@ -1,15 +1,78 @@
-import React from "react";
-import styled from "styled-components";
+import React, {use, useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const container = 1400;
+const PostShow = () => {
+    const {title} = useParams();
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-const ShowPost = () => {
+    // useEffect(() => {
+    //     const fetchPost = async () => {
+    //         try {
+    //             const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/posts/${id}`);
+    //             setPost(res.data);
+    //         } catch {
+    //             setError('포스트를 불러오는데 오류가 발생했습니다');
+    //             setTimeout(() => {
+    //                 window.location.reload();
+    //             },500);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     fetchPost();
+    // }, [id]);
+
+        useEffect(() => {
+      const fetchPost = async () => {
+        try {
+          const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/posts/title/${encodeURIComponent(title)}`);
+          setPost(res.data);
+        } catch (err) {
+          setError('포스트를 불러오는데 오류가 발생했습니다');
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchPost();
+    }, [title]);
+
+
+
+    if(loading) {
+        return (
+            <>
+                <p>로딩중</p>
+            </>
+        )
+    }
+    if(error) {
+        return (
+            <>
+                <p>{error}</p>
+            </>
+        )
+    }
+    if(!post) {
+        return (
+            <>
+                <p>포스트가 없습니다.</p>
+            </>
+        )
+    }
+
     return(
         <>
-            
+            <div>
+                <h2>{post.title}</h2>
+                <p>작성자: {post.id}</p>
+                <p>글: {post.content}</p>
+            </div>
         </>
     )
-}
+};
 
-export default ShowPost;
+export default PostShow;
