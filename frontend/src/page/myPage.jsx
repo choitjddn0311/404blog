1af7c51395c3eb401c8758e9535f80d03345d13b
choitjddn0311 <cswcjw311427@naver.com>
2025-06-24 15:02:25 +0900

    feat(myPage): add user info retrieval and display logic on MyPage using axios, with gender translation and birthday formatting

--- frontend/src/page/myPage.jsx
+++ frontend/src/page/myPage.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
+import axios from "axios";
+import moment from 'moment';
 
 const container = 1400;
 
@@ -16,10 +18,59 @@ const Container = styled.div`
 `
 
 const Mypage = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        // localstorge에서 id가져오고
+        const id = localStorage.getItem('userId');
+        // console.log(id)
+
+        if(!id) return;
+
+        // 여기서 백엔드에 넘김
+        axios.post(`${process.env.REACT_APP_API_URL}/user/info` , {id})
+            .then(res => {
+                if(res.data.success) {
+                    setUser(res.data.user);
+                }
+            })
+            .catch(err => {
+                console.error('정보 가져오기 실패' , err)
+            })
+    },[]);
+
+    // db에서 넘어오는거 case문으로 한글로 출력해줌
+    const getGenderText = (code) => {
+        switch(code) {
+            case 'male':
+                return '남성';
+            case 'female':
+                return '여성';
+            case 'other':
+                return '기타';
+            default:
+                return '미입력';
+        }
+    }
+
     return (
         <>
             <Main>
-                <Container></Container>
+                <Container>
+                    {user? (
+                        <>
+                            <h1>마이페이지</h1>
+                            <p>아아디 {user.id}</p>
+                            <p>이메일 {user.email}</p>
+                            {/* moment.js 를 활용한 datetime format */}
+                            <p>생년월일: {moment(user.birthday).format('YYYY년 MM월 DD일')}</p>
+                            {/* 성별 출력 */}
+                            <p>성별: {getGenderText(user.gender)}</p>
+                        </>
+                    ) : (
+                        <p>정보가져오는중</p>
+                    )}
+                </Container>
             </Main>
         </>
     )
