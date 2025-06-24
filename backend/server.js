2820c7c4d11e7d7e387366e65b517d94b001a248
choitjddn0311 <cswcjw311427@naver.com>
2025-06-24 15:00:04 +0900

    feat: 마이페이지 라우터 추가

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,28 @@ const { authPlugins } = require('mysql2');
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/post');
 const uploadRoutes = require('./routes/upload');
+const mypageRoutes = require('./routes/mypage');
 
 const app = express();
 const PORT = 5000;
 
 // app.use(cors());
 
+// 10.0.0.32에서 cors 허용
+// 나중에 이건 자세히 봐야할듯
 app.use(cors({
   origin: 'http://10.0.0.32:3000',
   credentials: true
 }));
 app.use(express.json());
 
+// 라우터 연결
 app.use('/api' , authRoutes);
 app.use('/api/posts' , postRoutes);
 app.use('/api/upload' , uploadRoutes);
+app.use('/user' , mypageRoutes);
 
 app.listen(PORT , '0.0.0.0', () => {
-  console.log(`서버 포트 ${PORT}여기서 돌아가는중`);
+  // console.log(`서버 포트 ${PORT}여기서 돌아가는중`);
 });
 
