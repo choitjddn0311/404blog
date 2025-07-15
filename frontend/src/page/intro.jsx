8a260725f4abf76ec9f50c7f6c6105cc5758092f
choitjddn0311 <cswcjw311427@naver.com>
2025-07-15 16:49:02 +0900

    리펙토링 예정

--- frontend/src/page/intro.jsx
+++ frontend/src/page/intro.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/a11y-dark.css";
 
 const Intro = () => {
-    const markdownText = `# h1 \n *만이러ㅣㅏㄴㅇㄹ* \n \`const app = use.get()\`하는거노?`
     return (
         <>
-            <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{markdownText}
-            </ReactMarkdown>
         </>
     )   
 }
