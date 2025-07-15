49aa0851e670459380f46e9c008f2390818c5851
choitjddn0311 <cswcjw311427@naver.com>
2025-07-15 18:26:41 +0900

    feat: 소개 페이지 이미지 추가

--- frontend/src/page/intro.jsx
+++ frontend/src/page/intro.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from "rehype-highlight";
+import Profile from "../assets/images/my/profile.png"
+import styled from 'styled-components';
+
+const Main = styled.main`
+    width: 100%;
+    height: 200vh;
+    display: flex;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 1400px;
+    height: 100%;
+    
+`
 
 const Intro = () => {
     return (
         <>
+        <Main>
+            <Container>
+                <img src={Profile} />
+            </Container>
+        </Main>
         </>
     )   
 }
